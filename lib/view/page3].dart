4fd3de652c80db5b552b0aa146e546173da37f65
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-07-02 12:37:22 +0500

    working

--- lib/view/page3].dart
+++ lib/view/page3].dart
@@ -16,12 +16,31 @@ class _Page3State extends State<Page3> {
           children: [
             Icon(Icons.arrow_back),
             SizedBox(width: 20,),
-            Text('Tankers Details',style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold,color: Colors.black),)
+            Text('Tanker details',style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold,color: Colors.black),)
           ],
         ),
         actions: [
           Container(
             child: IconButton(onPressed: (){}, icon: Icon(Icons.heart_broken)),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 20,),
+           Row(
+             children: [
+               SizedBox(width: 30,),
+               Text('Location',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 34,color: Colors.black),),
+             ],
+           ),
+          SizedBox(height: 20,),
+          Row(
+            children: [
+              SizedBox(width: 20,),
+              Text('Mirpur 11 Block c Dhaka\n Bangladesh'),
+
+            ],
           )
         ],
       ),
