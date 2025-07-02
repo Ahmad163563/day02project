a4cd4c98be935475aea13cff0c716bd6de64474b
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-07-02 17:19:15 +0500

    working

--- lib/view/page8.dart
+++ lib/view/page8.dart
@@ -21,7 +21,7 @@ class _Page9State extends State<Page8> {
             width: 300,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
-              color: Colors.grey[30]
+              color: Colors.grey[50]
             ),
             child: Padding(
               padding: const EdgeInsets.all(8.0),
@@ -39,16 +39,35 @@ class _Page9State extends State<Page8> {
               ),
             ),
           ),
+          SizedBox(height: 20,),
           Container(
             height: 200,
             width: 350,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
-              color: Colors.grey[30]
+              color: Colors.grey[50]
             ),
             child: Column(
               children: [
-                
+                //SizedBox(width: 10,
+                ),
+                Text('Estimated delivery time'),
+                Text('15-20 min'),
+                Divider(
+                  thickness: 2,
+                  endIndent: 50,
+                  indent: 50,
+                ),
+                 Padding(
+                   padding: const EdgeInsets.all(15),
+                   child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.black,
+                      child: Icon(Icons.location_on_outlined,color: Colors.white,),),
+                    title: Text('Sabboor Ali , Office',style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),),
+                    subtitle: Text('Lahore-Punjab'),
+                                   ),
+                 )
               ],
             ),
           )
